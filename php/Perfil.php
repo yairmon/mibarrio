fc6d2c74348cf55f8ddde075d354e82ec9861389
Yair Mondragon <blackandwhite654@gmail.com>
2014-09-14 11:31:36 -0500

    Nuevo commit, archivo perfil.php, mostrara informacion dependiendo de los permisos, es el menu

---
+++ php/Perfil.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Mi Barrio</title>
+<link rel="stylesheet" href="../css/perfil.css" media="screen" type="text/css" />
+</head>
+<body>
+
+<div class="menu-bar">
+	<div class="image-perfil">
+
+
+	</div>
+
+<?php
+
+echo "<h1>Nombre Usuario</h1><br>"; 
+
+/*aqui iran condiciones para ir agregando vainas, ejemplo ls links, Ej:
+
+if($permiso=$admon){
+
+echo "<a href='php/Contra.php'>Modificar Perfil</a>";
+
+}
+
+etc....
+
+
+*/
+
+echo "<div class='login-help'>";
+echo "<a href='php/Contra.php'>Modificar Perfil</a></div>";
+
+?>
+
+	
+
+
+
+</div>
+
+</body>
+</html>
